064f84d278985d3fb87c73fbdbde11927085dbec
goo-goo-goo-joob <pro1895sto@gmail.com>
2018-07-19 12:09:12 +0300

    add image generation

--- Calculate/views.py
+++ Calculate/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
+import subprocess
+import time
 
 
 def index(request):
     context = {}
     if request.method == 'POST':
         context['brackets'] = request.POST.get('brackets', None)
+        name = 'static/gen-img/%i.png' % int(time.time())
+        subprocess.call(['Cat_Br_Tr.exe', context['brackets'], 'Calculate/' + name])
+        context['img'] = name
     return render(request, 'calculate/index.html', context=context)
