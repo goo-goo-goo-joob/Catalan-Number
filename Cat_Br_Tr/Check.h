7b0c248b47b391c29e980927ab01c1d5887a10ae
goo-goo-goo-joob <pro1895sto@gmail.com>
2018-08-30 12:21:31 +0300

    fix bin tree linux

--- Cat_Br_Tr/Check.h
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include"Struct.h"
-#include<stdio.h>
-#include<locale.h>
-#include <string.h>
-using namespace std;
--- Cat_Br_Tr/Struct.h
+++
@@ -1,5 +0,0 @@
-#pragma once
-typedef struct {
-	int key;
-	struct tnode *left, *right, *parent;
-} tnode;
